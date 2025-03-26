d5f738586c017ff3a812d4bed442254637d9493c
Zipho-Home <zluvuno@gmail.com>
2025-03-26 14:18:57 +0200

    Whatsapp Button intergration

--- assets/js/main.js
+++ assets/js/main.js
@@ -334,3 +334,12 @@ document.addEventListener('DOMContentLoaded', () => {
   // Initial call to set position
   updateSidebarPosition();
 });
+
+  // ToolTi
+
+document.addEventListener("DOMContentLoaded", function () {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl);
+    });
+});
